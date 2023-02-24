50177dac28b7e89e5acf261a1acc74df7d91bbd7
Samyak1202 <99115614+Samyak1202@users.noreply.github.com>
2023-02-24 16:54:04 +0530

    Update settings.py
    remove secret key

--- djangoauth1/djangoauth1/settings.py
+++ djangoauth1/djangoauth1/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iw15n*6^y#@%0rdevclhbtyb#sb3_z&j$b#0nc9xv@7r0=tqm4'
+SECRET_KEY = '*****'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
